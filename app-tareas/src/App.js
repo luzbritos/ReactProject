6df17448e728120ea4179e4a33ff85f742f6cb27
lbritos <lbritos@tecnoaccion.com.ar>
2022-11-28 12:59:41 -0300

    App Tareas

---
+++ app-tareas/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ListaDeTareas from './components/ListaDeTareas';
+
+
+
+function App() {
+  return (
+    <div className='aplicacion-tareas'>
+      <div className='tareas-lista-principal' >
+        <h1>Mis tareas</h1>
+        <ListaDeTareas/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
