c791c964d7038023acd7eafefd418ff3fcc52cb4
Maria Luz Britos <84516303+marialuz1982@users.noreply.github.com>
2023-01-15 13:35:42 -0300

    Sidebar

---
+++ sidebar/src/context.js
@@ -0,0 +1,37 @@
+import React, { useState, useContext } from 'react'
+
+const Appcontext = React.createContext()
+
+const AppProvider = ({children}) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+
+    const openModal = () => {
+        setIsModalOpen(true)
+    }
+    const closeModal = () => {
+        setIsModalOpen(false)
+    }
+
+    return(
+        <Appcontext.Provider value={{
+            isModalOpen,isSidebarOpen,openModal,openSidebar,closeModal,closeSidebar
+        }} >{children}</Appcontext.Provider>
+    )
+}
+//custom hooks
+export const useGlobalContext = () => {
+    return(
+        useContext(Appcontext)
+    )
+}
+
+
+export {AppProvider,Appcontext}
