7c196f35af2a5361a6fb926359a4a21da3640e22
lbritos <lbritos@tecnoaccion.com.ar>
2023-03-01 12:39:16 -0300

    Wordle

---
+++ wordle/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+import {createContext, useState} from 'react';
+import { boardDefault } from './components/Words';
+
+export const AppContext = createContext()
+
+function App() {
+  const [board, setBoard] = useState(boardDefault)
+  return (
+    <div className="App">
+     <nav>
+      <h1>Wordle</h1>
+     </nav>
+     <AppContext.Provider value = {{board, setBoard}}> 
+     <Board/>
+     <Keyboard/>
+     </AppContext.Provider>
+     
+    </div>
+  );
+}
+
+export default App;
